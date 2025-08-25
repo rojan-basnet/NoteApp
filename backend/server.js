e0f1214603e24015c689c7846637e55a17818dd3
Rojan Basnet <rojanbasnet072@gmail.com>
2025-08-25 18:29:42 +0545

    password hashing added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDb from './config/db.js';
 import { note } from './models/note.js';
 import { NoteBody } from './models/noteBody.js';
 import path from 'path'
+import bcrypt from 'bcrypt'
 
 const PORT=process.env.PORT||5000;
 const __dirname=path.resolve()
@@ -23,7 +24,8 @@ app.post('/api/createNewUser',async (req,res)=>{
     try{
         const existingUserName=await newUser.findOne({userName});
         if(!existingUserName){
-            const newCreatedUser = new newUser({userName,password})
+            const hashedPassword= await bcrypt.hash(password,10)
+            const newCreatedUser = new newUser({userName,hashedPassword})
             await newCreatedUser.save()
             return res.status(201).json({message:"new user created",data:newCreatedUser})
         }
@@ -38,7 +40,8 @@ app.post('/api/login',async (req,res)=>{
     const user=await newUser.findOne({userName:userName})
 
     if(user){
-        if(user.password==password){
+        if(await bcrypt.compare(password,user.password)){
+            console.log(await bcrypt.compare(password,user.password))
             return res.status(200).json({message:"user logged in",data:user})
         }
         return res.status(401).json("incorrect password")
